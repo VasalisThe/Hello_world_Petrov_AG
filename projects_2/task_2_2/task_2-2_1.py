736812c25ea718f951246088f6a92365b9ef4d13
Петров Алексей <48839460+VasalisThe@users.noreply.github.com>
2026-02-10 23:16:43 +0300

    Upload task2_2

---
+++ projects_2/task_2_2/task_2-2_1.py
@@ -0,0 +1,6 @@
+course_name = 'Informatics'
+current_stage = '2-2_1'
+count_of_completed_tasks = 10
+
+print(f'Course name: {course_name}\nCurrent stage: {current_stage}\n' \
+      'Count of completed tasks: {count_of_completed_tasks}')
